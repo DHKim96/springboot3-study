f8981934bb63d04bf66e602623284e281f3efbe4
DHKim96 <history8549@gmail.com>
2025-10-15 23:50:34 +0900

    feat. 리프레시토큰 구현중2

--- src/main/java/me/shinsunyoung/springbootdeveloper/repository/RefreshTokenRepository.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/repository/RefreshTokenRepository.java
@@ -1,4 +1,12 @@
 package me.shinsunyoung.springbootdeveloper.repository;
 
+import me.shinsunyoung.springbootdeveloper.domain.RefreshToken;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
 public interface RefreshTokenRepository {
+    Optional<RefreshToken> findByUserId(Long userId); // Optional로 감싸서 null-safety를 보장
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
