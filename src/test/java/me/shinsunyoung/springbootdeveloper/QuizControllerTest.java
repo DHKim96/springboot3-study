56e5fab1edbf7189c35fd80f6c13fd9b3451d0be
DHKim96 <history8549@gmail.com>
2026-01-26 22:35:53 +0900

    테스트 세팅

---
+++ src/test/java/me/shinsunyoung/springbootdeveloper/QuizControllerTest.java
@@ -0,0 +1,32 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class QuizControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void mockMvcSetup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+}
---
+++ src/test/java/me/shinsunyoung/springbootdeveloper/TestControllerTest.java
@@ -0,0 +1,58 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TestControllerTest {
+    @Autowired
+    protected MockMvc mockmvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp(){
+        this.mockmvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @AfterEach
+    public void cleanUp(){
+        memberRepository.deleteAll();
+    }
+
+    @DisplayName("getAllMembers: 아티클 조회에 성공한다.")
+    @Test
+    public void getAllMembers() throws Exception {
+        //given : 멤버 저장
+        final String url = "/test";
+        Member savedMember = memberRepository.save(new Member(1L, "홍길동"));
+        // when : 멤버 리스트 조회하는 API 호출
+        final ResultActions result = mockmvc.perform(get(url).accept(MediaType.APPLICATION_JSON));
+        // then : 응답 코드가 200 OK 이고 반환받은 값 중 0번째 요소의 id, name 이 저장된 값과 같은지 확인
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(savedMember.getId()))
+                .andExpect(jsonPath("$[0].name").value(savedMember.getName()));
+    }
+
+
+}
